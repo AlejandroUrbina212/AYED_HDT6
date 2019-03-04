c1327c34776cab85f765a401f9f4514660ce60a7
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-03-03 23:32:21 -0600

    Add files via upload

---
+++ Factory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HDT6;
+
+/**
+ *
+ * @author DIANA
+ */
+public class Factory {
+    
+}
