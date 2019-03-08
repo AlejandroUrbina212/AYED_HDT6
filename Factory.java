4a749de85de551d76b67c347267e0054c4d6eaad
Luis Urbina <luis212urbina@gmail.com>
2019-03-08 11:46:37 -0600

    Add files via upload

--- Factory.java
+++ Factory.java
@@ -1,19 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HDT6;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * Class to create a different list depending on the user´s request (as a string)
+ *
  * @author DIANA
  */
 public class Factory {
-    /**
-     * Returns an instance of the map selected by the user (as a String)
-     * @param map the string of the map the user will choose
-     * @return one of three posible instance of a Map-type Object
-     */
-    public static Map<String,String> getMap(String map){
+    public Map<String,String> getMap(String map){
         switch (map) {
             case "HASHMAP":
                 return new HashMap<>();
@@ -26,13 +28,7 @@ public class Factory {
         }
         return null;
     }
-
-    /**
-     * Returns an instance of a map for control of the quantity of cards the user has
-     * @param map the string of the desired map
-     * @return the instance of the Map-type object according to the param
-     */
-    public static Map<String,Integer> getMapCant(String map){
+    public Map<String,Integer> getMapCant(String map){
         switch (map) {
             case "HASHMAP":
                 return new HashMap<>();
